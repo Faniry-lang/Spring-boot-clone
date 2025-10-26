70ed826d9a7a24e8f856086c6ebf76650bb53f68
Faniry-lang <faniryranaivoson167@gmail.com>
2025-10-26 13:37:41 +0300

    Added @Controller annotation and implemented class path scanning

--- test_project_new/src/main/java/com/test/Main.java
+++ test_project_new/src/main/java/com/test/Main.java
@@ -1,11 +1,15 @@
 package com.test;
 
 import java.lang.reflect.Method;
+import java.util.List;
 
+import com.itu.framework.annotations.Controller;
 import com.itu.framework.annotations.URL;
+import com.itu.framework.helpers.ComponentScan;
 
 public class Main {
     public static void main(String[] args) {
+        /*
         System.out.println("Scanning for methods annotated with @URL...");
         Class<URLTest> clazz = URLTest.class;
         for (Method method : clazz.getDeclaredMethods()) {
@@ -13,6 +17,13 @@ public class Main {
                 URL urlAnnotation = method.getAnnotation(URL.class);
                 System.out.println("Found annotated method: " + method.getName() + " with URL: " + urlAnnotation.value());
             }
+        }
+        */
+        Class<Controller> controllerClass = Controller.class;
+        List<Class<?>> annotatedControllerClasses = ComponentScan.getAnnotatedClassesByAnnotation(controllerClass);
+        for(Class clazz : annotatedControllerClasses)
+        {
+            System.out.println(clazz.toString());
         }
     }
 }
